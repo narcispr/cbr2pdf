ac40e951c2f17e9a7a78a8287ebf04d13162302b
Narcis Palomeras <narcispr@gmail.com>
2024-04-13 15:46:40 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# CBR to PDF scrip
+
+This script is used to convert CBR files to PDF files. 
+
+## Requirements
+
+* Python 3.6 or higher
+* Pilow Image Library
+* fpdf library
+* argparse library
+* tqdm library
+
+To install all the requirements, install firts Python 3.6 or higher and the run the following command:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+CBR files are compressed files that contain images. Rename first the CBR file to `*.zip` and then descompress it. This script has to be pointed (`path` parameter) to the folder containing the images.
+
+```bash
+usage: cbr2pdf.py [-h] [--path PATH] [--bw BW]
+                  [--contrast CONTRAST] [--brightness BRIGHTNESS]
+                  [--width WIDTH] [--height HEIGHT]
+                  [--crop_left CROP_LEFT] [--crop_right CROP_RIGHT]
+                  [--crop_top CROP_TOP] [--crop_bottom CROP_BOTTOM]
+                  [--output OUTPUT]
+
+```
+Example:
+
+```bash
+python.exe .\cbr2pdf.py --path '..\Dragon Ball\Dragon Ball 1' --bw True --contrast True --brightness 1.2 --width 1072 --height 1448 --crop_bottom 20 --output dragon_ball_001.pdf
+```
+
+Set width and height to the screen size of your device. 
+
+
+---
+Narcís Palomeras
+April 2024
---
+++ cbr2pdf.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+from PIL import Image, ImageOps, ImageEnhance
+from fpdf import FPDF
+import argparse
+from tqdm import tqdm
+
+
+def get_files(path='.'):
+    "Get all files in given folder"
+    os.chdir(path)
+    return [f for f in os.listdir() if os.path.isfile(f)]
+
+
+def correction(f, bw=False, contrast=True, brightness=1.0, resize=None, crop=(0,0,0,0)):
+    "Given a file, open it with PIL and apply a brightness and contrast filter and save it as a new file"
+    im = Image.open(f)
+    if bw:
+        im = ImageOps.grayscale(im)
+    if crop != (0,0,0,0):
+        im = im.crop((crop[0], crop[1], im.width - crop[2], im.height - crop[3]))
+
+    if contrast:
+        im = ImageOps.autocontrast(im, cutoff=3.0)
+    if brightness != 1.0:
+        # apply brightness filter
+        enhancer = ImageEnhance.Brightness(im)
+        im = enhancer.enhance(brightness)
+
+    if resize is not None:
+        im = im.resize((resize[0], resize[1]), Image.Resampling.BICUBIC)
+    im.save(os.path.join("tmp/" + f))
+
+
+def jpg_to_pdf(files, output="output.pdf"):
+    "Given a list of files, create a pdf with them"
+    pdf = FPDF()
+    print("Creating pdf...")
+    os.chdir("tmp")
+    for f in tqdm(files):
+        # print progress bar with i out of len(files)
+        pdf.add_page()
+        pdf.image(f, 0, 0, 210, 297)
+    print("Saving pdf...")
+    pdf.output(output)
+
+def cbr2pdf(path, bw=False, contrast=True, brightness=1.0, resize=None, crop=(0,0,0,0), output="output.pdf"):
+    "Given a folder with image files, process them and convert them to pdf"
+    # get current folder
+    current_path = os.getcwd()
+
+    # print("current path: ", current_path)
+    # print("path: ", path)
+    # print("output: ", output)
+    # print("bw: ", bw)
+    # print("contrast: ", contrast)
+    # print("brightness: ", brightness)
+    # print("resize: ", resize)
+    # print("crop: ", crop)
+
+    files = get_files(path)
+    print("Processing files...")
+    # create tmp folder
+    try:
+        os.mkdir("tmp")
+    except FileExistsError:
+        pass
+    for f in tqdm(files):
+        correction(f, bw=bw, contrast=contrast, brightness=brightness, resize=resize, crop=crop)
+    jpg_to_pdf(files, os.path.join(current_path, output))
+
+    # remove folder and its content files
+    for f in files:
+        os.remove(f)
+    os.chdir("..")
+    os.rmdir("tmp")
+
+if __name__ == "__main__":
+    # get parameters from command line
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--path', type=str, help='Path to the file')
+    parser.add_argument('--bw', type=str, default="False", help='Black and white filter')
+    parser.add_argument('--contrast', type=str, default="True", help='Automatic contrast filter')
+    parser.add_argument('--brightness', type=float, default=1.0, help='Change brightness of the image (e.g., 1.3)')
+    parser.add_argument('--width', type=int, default=None, help='Device width in pixels (Kindle 11th: 1072)')
+    parser.add_argument('--height', type=int, default=None, help='Device height in pixels (Kindle 11th: 1448)')
+    parser.add_argument('--crop_left', type=int, default=0, help='Crop original image left side in pixels')
+    parser.add_argument('--crop_right', type=int, default=0, help='Crop original image right side in pixels')
+    parser.add_argument('--crop_top', type=int, default=0, help='Crop original image top side in pixels')
+    parser.add_argument('--crop_bottom', type=int, default=0, help='Crop original image bottom side in pixels')
+    parser.add_argument('--output', type=str, default='output.pdf', help='Output file name')
+    args = parser.parse_args()
+    if args.width is not None and args.height is not None:
+        resize = (args.width, args.height)
+    else:
+        resize = None
+ 
+    cbr2pdf(args.path, resize=resize, bw=(args.bw=="True"), contrast=(args.contrast=="True"), brightness=args.brightness, 
+            crop = (args.crop_left, args.crop_top, args.crop_right, args.crop_bottom), output=args.output)
+
+
